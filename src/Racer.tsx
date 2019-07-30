c4e54ed4f5df0cecb1aed74659c7d441d95693d8
electronik54 <electro_nik_54@hotmail.com>
2019-07-30 15:10:04 -0400

    added render movecars and init race

--- src/Racer.tsx
+++ src/Racer.tsx
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import update from 'immutability-helper';
 
 interface Props { }
 interface State {
   racers: {
-    name: String
-    , maxSpeed: Number
-    , position: Number | null
-    , carMake: String
-    , carBreakdownChance: Number
-    , carNitroChance: Number
+    readonly name: String
+    , readonly maxSpeed: number
+    , position: {
+      css: number,
+      world: number
+    }
+    , readonly carMake: String
+    , readonly carBreakdownChance: number
+    , readonly carNitroChance: number
+    , canRace: boolean
   }[]
-  , track: {
-    length: Number
+  , readonly track: {
+    length: number
     , name: String
     , laps: number
+  }
+  , raceTimer: {
+    timer: NodeJS.Timeout | null
+    , interval: number
   }
 }
 
@@ -30,38 +38,105 @@ export default class Racer extends React.Component<Props, State>{
         {
           name: ""
           , maxSpeed: 0
-          , position: 10
+          , position: {
+            css: 0,
+            world: 0
+          }
           , carMake: ""
           , carBreakdownChance: 0
           , carNitroChance: 0
+          , canRace: true
         }
         , {
           name: ""
           , maxSpeed: 0
-          , position: 10
+          , position: {
+            css: 0,
+            world: 0
+          }
           , carMake: ""
           , carBreakdownChance: 0
           , carNitroChance: 0
+          , canRace: true
         }
       ]
       , track: {
         length: 3000, name: "", laps: 2
       }
+      , raceTimer: {
+        timer: null
+        , interval: 1000
+      }
     }
 
+    this.initilizeRace();
 
+  }
+
+
+  initilizeRace = (): void => {
+    console.log(`race started`);
+    this.startRace();
   }
 
   startRace = () => {
 
+    let tm = setInterval(() => {
+      this.moveRacers(this.state);
+    }, this.state.raceTimer.interval);
+
+    debugger
+    const raceTimer = update(this.state, {
+      raceTimer: {timer: { $set: tm }}
+    });
+    this.setState(
+      raceTimer
+    );
+
+  }
+
+  moveRacers = (st: State): void => {
+
+    let racers = [...st.racers]
+      , trackLength = st.track.length;
+
+    // racers[0].name == "sdf";
+    racers.forEach((racer, index) => {
+
+      debugger
+      racer.position.world += racer.maxSpeed;
+      if (racer.position.world >= trackLength) {
+        const canRace = update(this.state.racers, {
+          index: { canRace: { $set: false } } ,
+        });
+
+        this.setState({
+          racers: canRace
+        });
+      }
+    });
+
   }
 
 
-  render() {
+  render(): JSX.Element {
+
+    const racer1Style = {
+      marginLeft: `${this.state.racers[0].position.css}%`
+    };
 
     return (
-      <section>
-        <h1></h1>
+
+      <section className="raceContainer">
+        <h1>{this.state.track.name}</h1>
+
+        <article>
+          <div className="racerContainer" style={racer1Style}>racer1</div>
+          <div className="racerContainer" style={racer1Style}>racer2</div>
+          <div className="racerContainer" style={racer1Style}>racer3</div>
+        </article>
+
+
       </section>
 
     );
